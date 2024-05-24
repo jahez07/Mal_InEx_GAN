2b07765def596f4fd964f93fbdbf5310c70af991
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-05-24 15:48:37 +0400

    Update GAN_Inception.py

--- scripts/GAN_Inception.py
+++ scripts/GAN_Inception.py
@@ -12,11 +12,16 @@ cur_dir = os.getcwd() # getting the current directory
 os.chdir(imagedir)  # the parent folder with sub-folders
 
 # Get number of samples per family
-list_fams = sorted(os.listdir(os.getcwd()), key=str.lower)  # vector of strings with family names
+
+# vector of strings with family names
+list_fams = sorted(os.listdir(os.getcwd()), key=str.lower)
 no_imgs = []  # No. of samples per family
 for i in range(len(list_fams)):
     os.chdir(list_fams[i])
-    len1 = len(glob.glob('*.png'))  # assuming the images are stored as 'png'
+    # assuming the images are stored as 'png'
+    len1 = len(glob.glob('*.png'))
     no_imgs.append(len1)
     os.chdir('..')
-num_samples = np.sum(no_imgs)  # total number of all samples
+# total number of all samples
+num_samples = np.sum(no_imgs)
+
