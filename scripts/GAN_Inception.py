cf326e126192f5cc9a887962b359ab456d11b41a
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-05-27 17:05:26 +0400

    Update GAN_Inception.py

--- scripts/GAN_Inception.py
+++ scripts/GAN_Inception.py
@@ -199,6 +199,72 @@ def generate_fake_samples(generator, latent_dim, n_samples):
 	y = zeros((n_samples, 1))  #Label=0 indicating they are fake
 	return X, y
 
+# Train the generator and discriminator
+# We loop through a number of epochs to train our Discriminator by first selecting
+# a random batch of images fromv our true/real dataset.
+# Then, generating a set of images using the generator.
+# Feed both set of images into the Discriminator.
+# Finally, set the loss parameters for both the real and fake images, as well as the combined loss.
+Gen_Loss = []
+def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batch=10):
+
+  #the discriminator model is updated for a half batch of real samples
+  # and a half batch of fake samples, combined a single batch.
+  bat_per_epo = int(dataset.shape[0] / n_batch)
+  half_batch = int(n_batch / 2)
+
+  # manually enumerate epochs and bacthes.
+  for i in range(n_epochs):
+
+    # enumerate batches over the training set
+    for j in range(bat_per_epo):
+
+      # Train the discriminator on real and fake images, separately (half batch each)
+      # Research showed that separate training is more effective.
+			# get randomly selected 'real' samples
+      X_real, y_real = generate_real_samples(dataset, half_batch)
+
+      # Update discriminator model weights
+      # Train_on_batch allows you to update weights based on a collection
+      # of samples you provide
+      # Let us just capture loss and ignore accuracy value (2nd output below)
+      d_loss_real, _ = d_model.train_on_batch(X_real, y_real)
+
+      # Generate 'fake' examples
+      X_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
+
+      # Update discriminator model weights
+      d_loss_fake, _ = d_model.train_on_batch(X_fake, y_fake)
+
+      # d_loss = 0.5 * np.add(d_loss_real, d_loss_fake) #Average loss if you want to report single..
+
+			# Prepare points in latent space as input for the generator
+      X_gan = generate_latent_points(latent_dim, n_batch)
+      #print(X_gan.shape)
+
+      # The generator wants the discriminator to label the generated samples
+      # as valid (ones)
+      # This is where the generator is trying to trick discriminator into believing
+      # the generated image is true (hence value of 1 for y)
+      y_gan = ones((n_batch, 1))
+
+      # Generator is part of combined model where it got directly linked with the discriminator
+      # Train the generator with latent_dim as x and 1 as y.
+      # Again, 1 as the output as it is adversarial and if generator did a great
+      # job of folling the discriminator then the output would be 1 (true)
+			# update the generator via the discriminator's error
+      g_loss = gan_model.train_on_batch(X_gan, y_gan)
+
+      # Print losses on this batch
+      print('Epoch>%d, Batch %d/%d, d1=%.3f, d2=%.3f g=%.3f' %
+       (i+1, j+1, bat_per_epo, d_loss_real, d_loss_fake, g_loss))
+
+      #if i % 20 == 0:
+       #     prediction_model(i)
+      #Gen_Loss.append(g_loss)
+
+# save the generator model
+  g_model.save('/content/drive/MyDrive/Jahez_Vinod_2023/MalHub/Models/GAN[adload]_generator.h5')
 
 
 #              G E N E R A T I N G  S A M P L E S
