5f9b381f617008f20560be8a297e9c60a3b97ff4
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-05-28 20:50:01 +0400

    Update GAN_Inception.py

--- scripts/GAN_Inception.py
+++ scripts/GAN_Inception.py
@@ -1,12 +1,12 @@
 import os
-import numpy as np
 import glob
-from keras.models import Sequential
-from keras.layers import Dense, Reshape, LeakyReLU, Conv2DTranspose, Conv2D, BatchNormalization, Flatten, Dropout
-from keras.optimizers import Adam
-from numpy.random import randint, randn
+import numpy as np
 from numpy import ones, zeros
+from keras.optimizers import Adam
 from keras.models import load_model
+from keras.models import Sequential
+from numpy.random import randint, randn
+from keras.layers import Dense, Reshape, LeakyReLU, Conv2DTranspose, Conv2D, BatchNormalization, Flatten, Dropout
 
 
 #              E X T R A C T I N G  &  I N I T I A L I Z I N G  L A B E L S
